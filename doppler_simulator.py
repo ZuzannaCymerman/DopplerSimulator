295ce089dfc05ceef070e6fe999ed52d43a00080
Zuzanna Cymerman <zuzannacymerman@Zuzannas-MacBook-Air.local>
2022-11-22 23:20:15 +0100

    trying to create a wideband wave

--- doppler_simulator.py
+++ doppler_simulator.py
@@ -1,22 +1,35 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.fftpack as fftpack # We use this one to perform Fourier transforms
+import random
 
+def f(x):
+    npx = np.sin(x)
+    for i in range(3):
+        r = random.randint(8,10)*0.1
+        # npx = npx + np.sin(r*x)
+    return npx
+    
 if __name__ == "__main__":
+    x = np.linspace(0, 10000)
 
-    # x axis values
-    x = [1,2,3]
-    # corresponding y axis values
-    y = [2,4,1]
+    plt.plot(x, f(x))
+    plt.show()
+    # # x axis values
+    # x = [1,2,3]
+    # # corresponding y axis values
+    # y = [2,4,1]
     
-    # plotting the points 
-    plt.plot(x, y)
+    # # plotting the points 
+    # plt.plot(x, y)
     
-    # naming the x axis
-    plt.xlabel('x - axis')
-    # naming the y axis
-    plt.ylabel('y - axis')
+    # # naming the x axis
+    # plt.xlabel('x - axis')
+    # # naming the y axis
+    # plt.ylabel('y - axis')
     
-    # giving a title to my graph
-    plt.title('My first graph!')
+    # # giving a title to my graph
+    # plt.title('My first graph!')
     
-    # function to show the plot
-    plt.show()	 
+    # # function to show the plot
+    # plt.show()	 
